554ec7c1e94b439665153cfd981fbfd000e3872d
Christian Riedel <cielquan@protonmail.com>
2021-03-17 18:32:09 +0100

    copy functions from db to db::util

---
+++ rust_backend/src/db/util.rs
@@ -0,0 +1,17 @@
+use diesel::prelude::*;
+use dotenv::dotenv;
+use std::env;
+
+pub fn establish_connection() -> SqliteConnection {
+    dotenv().ok();
+
+    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+    SqliteConnection::establish(&database_url)
+        .unwrap_or_else(|_| panic!("Error connecting to {}", database_url))
+}
+
+no_arg_sql_function!(
+    last_insert_rowid,
+    diesel::sql_types::Integer,
+    "Represents the SQL last_insert_row() function"
+);
