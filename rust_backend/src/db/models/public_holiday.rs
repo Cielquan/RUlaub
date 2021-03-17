a5b9043fe072073447c0827cee12e959f131be39
Christian Riedel <cielquan@protonmail.com>
2021-03-17 19:32:56 +0100

    add Display/Debug impl for PublicHoliday model

--- rust_backend/src/db/models/public_holiday.rs
+++ rust_backend/src/db/models/public_holiday.rs
@@ -1,20 +1,32 @@
+use std::fmt::{self, Display, Formatter};
+
 use chrono::NaiveDate;
+use diesel::prelude::*;
+use diesel::result::Error;
+use tracing::{debug, instrument, trace};
 
-use super::super::schema::public_holidays;
+use super::super::{schema::public_holidays, util::last_insert_rowid};
 
-#[derive(Queryable)]
+#[derive(Queryable, Debug)]
 pub struct PublicHoliday {
     pub id: i32,
     pub state_id: i32,
     pub name: String,
     pub date: NaiveDate,
 }
-#[derive(Insertable)]
-#[table_name = "public_holidays"]
-pub struct NewPublicHoliday<'a> {
-    pub state_id: &'a i32,
-    pub name: &'a str,
-    pub date: &'a NaiveDate,
+
+impl Display for PublicHoliday {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        if f.alternate() {
+            write!(f, "<PublicHoliday {}>", self.name)
+        } else {
+            write!(
+                f,
+                "<PublicHoliday {} (date: {} | State-ID: {} | ID: {})>",
+                self.name, self.state_id, self.date, self.id
+            )
+        }
+    }
 }
 
 impl PublicHoliday {
@@ -28,5 +40,42 @@ impl PublicHoliday {
             name,
             date,
         }
+    }
+}
+
+#[derive(Insertable, Debug)]
+#[table_name = "public_holidays"]
+pub struct NewPublicHoliday<'a> {
+    pub state_id: &'a i32,
+    pub name: &'a str,
+    pub date: &'a NaiveDate,
+}
+
+impl Display for NewPublicHoliday<'_> {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        if f.alternate() {
+            write!(f, "<NewPublicHoliday {}>", self.name)
+        } else {
+            write!(
+                f,
+                "<NewPublicHoliday {} (date: {} | State-ID: {})>",
+                self.name, self.date, self.state_id
+            )
+        }
+    }
+}
+
+impl NewPublicHoliday<'_> {
+    #[instrument(skip(self, conn))]
+    pub fn save_to_db(self, conn: &SqliteConnection) -> Result<i32, Error> {
+        debug!(target: "new_db_entry", "Adding to db: {:?}", &self);
+        diesel::insert_into(public_holidays::table)
+            .values(&self)
+            .execute(conn)?;
+
+        trace!(target: "new_db_entry", "Get `last_insert_rowid` for id of: {:#}", &self);
+        let id = diesel::select(last_insert_rowid).get_result::<i32>(conn);
+        debug!(target: "new_db_entry", "Got ID: <{:?}> for {:#}", id, &self);
+        id
     }
 }
