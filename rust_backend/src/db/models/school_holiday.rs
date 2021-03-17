30e19854096747e5244291a7cd73c431d95059fc
Christian Riedel <cielquan@protonmail.com>
2021-03-17 19:33:27 +0100

    add Display/Debug impl for SchoolHoliday model

--- rust_backend/src/db/models/school_holiday.rs
+++ rust_backend/src/db/models/school_holiday.rs
@@ -1,8 +1,13 @@
+use std::fmt::{self, Display, Formatter};
+
 use chrono::NaiveDate;
+use diesel::prelude::*;
+use diesel::result::Error;
+use tracing::{debug, instrument, trace};
 
-use super::super::schema::school_holidays;
+use super::super::{schema::school_holidays, util::last_insert_rowid};
 
-#[derive(Queryable)]
+#[derive(Queryable, Debug)]
 pub struct SchoolHoliday {
     pub id: i32,
     pub state_id: i32,
@@ -11,14 +16,24 @@ pub struct SchoolHoliday {
     pub end_date: NaiveDate,
     pub comment: Option<String>,
 }
-#[derive(Insertable)]
-#[table_name = "school_holidays"]
-pub struct NewSchoolHoliday<'a> {
-    pub state_id: &'a i32,
-    pub type_id: &'a i32,
-    pub start_date: &'a NaiveDate,
-    pub end_date: &'a NaiveDate,
-    pub comment: Option<&'a str>,
+
+impl Display for SchoolHoliday {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        if f.alternate() {
+            write!(f, "<SchoolHoliday {} in {}>", self.type_id, self.state_id)
+        } else {
+            write!(
+                f,
+                "<SchoolHoliday {} in {} (Start Date: {} | End Date: {} | Comment {:?} | ID: {})>",
+                self.type_id,
+                self.state_id,
+                self.start_date,
+                self.end_date,
+                self.comment,
+                self.id
+            )
+        }
+    }
 }
 
 impl SchoolHoliday {
@@ -36,5 +51,44 @@ impl SchoolHoliday {
             end_date,
             comment,
         }
+    }
+}
+
+#[derive(Insertable, Debug)]
+#[table_name = "school_holidays"]
+pub struct NewSchoolHoliday<'a> {
+    pub state_id: &'a i32,
+    pub type_id: &'a i32,
+    pub start_date: &'a NaiveDate,
+    pub end_date: &'a NaiveDate,
+    pub comment: Option<&'a str>,
+}
+
+impl Display for NewSchoolHoliday<'_> {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        if f.alternate() {
+            write!(f, "<SchoolHoliday {} in {}>", self.type_id, self.state_id)
+        } else {
+            write!(
+                f,
+                "<SchoolHoliday {} in {} (Start Date: {} | End Date: {} | Comment {:?})>",
+                self.type_id, self.state_id, self.start_date, self.end_date, self.comment
+            )
+        }
+    }
+}
+
+impl NewSchoolHoliday<'_> {
+    #[instrument(skip(self, conn))]
+    pub fn save_to_db(self, conn: &SqliteConnection) -> Result<i32, Error> {
+        debug!(target: "new_db_entry", "Adding to db: {:?}", &self);
+        diesel::insert_into(school_holidays::table)
+            .values(&self)
+            .execute(conn)?;
+
+        trace!(target: "new_db_entry", "Get `last_insert_rowid` for id of: {:#}", &self);
+        let id = diesel::select(last_insert_rowid).get_result::<i32>(conn);
+        debug!(target: "new_db_entry", "Got ID: <{:?}> for {:#}", id, &self);
+        id
     }
 }
