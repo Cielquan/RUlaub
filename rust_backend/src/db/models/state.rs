f2aa69bf918a53f32215d44e2e439e59d4082c70
Christian Riedel <cielquan@protonmail.com>
2021-03-17 19:34:32 +0100

    add Display/Debug impl for State model

--- rust_backend/src/db/models/state.rs
+++ rust_backend/src/db/models/state.rs
@@ -1,16 +1,16 @@
-use super::super::schema::states;
+use std::fmt::{self, Display, Formatter};
 
-#[derive(Queryable)]
+use diesel::prelude::*;
+use diesel::result::Error;
+use tracing::{debug, instrument, trace};
+
+use super::super::{schema::states, util::last_insert_rowid};
+
+#[derive(Queryable, Debug)]
 pub struct State {
     pub id: i32,
     pub state_abbr: String,
     pub state_full: String,
-}
-#[derive(Insertable)]
-#[table_name = "states"]
-pub struct NewState<'a> {
-    pub state_abbr: &'a str,
-    pub state_full: &'a str,
 }
 
 impl State {
@@ -19,5 +19,51 @@ impl State {
             state_abbr,
             state_full,
         }
+    }
+}
+
+impl Display for State {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        if f.alternate() {
+            write!(f, "<State {}>", self.state_full)
+        } else {
+            write!(
+                f,
+                "<State {} (Abbr: {} | ID: {})>",
+                self.state_full, self.state_abbr, self.id
+            )
+        }
+    }
+}
+
+#[derive(Insertable, Debug)]
+#[table_name = "states"]
+pub struct NewState<'a> {
+    pub state_abbr: &'a str,
+    pub state_full: &'a str,
+}
+
+impl Display for NewState<'_> {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        if f.alternate() {
+            write!(f, "<State {}>", self.state_full)
+        } else {
+            write!(f, "<State {} (Abbr: {})>", self.state_full, self.state_abbr)
+        }
+    }
+}
+
+impl NewState<'_> {
+    #[instrument(skip(self, conn))]
+    pub fn save_to_db(self, conn: &SqliteConnection) -> Result<i32, Error> {
+        debug!(target: "new_db_entry", "Adding to db: {:?}", &self);
+        diesel::insert_into(states::table)
+            .values(&self)
+            .execute(conn)?;
+
+        trace!(target: "new_db_entry", "Get `last_insert_rowid` for id of: {:#}", &self);
+        let id = diesel::select(last_insert_rowid).get_result::<i32>(conn);
+        debug!(target: "new_db_entry", "Got ID: <{:?}> for {:#}", id, &self);
+        id
     }
 }
