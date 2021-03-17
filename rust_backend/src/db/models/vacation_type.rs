bfaa0221cba1427f22e3ac27592a365d2fcfee1a
Christian Riedel <cielquan@protonmail.com>
2021-03-17 19:35:19 +0100

    add Display/Debug impl for VacationType model

--- rust_backend/src/db/models/vacation_type.rs
+++ rust_backend/src/db/models/vacation_type.rs
@@ -1,20 +1,70 @@
-use super::super::schema::vacation_types;
+use std::fmt::{self, Display, Formatter};
 
-#[derive(Queryable)]
+use diesel::prelude::*;
+use diesel::result::Error;
+use tracing::{debug, instrument, trace};
+
+use super::super::{schema::vacation_types, util::last_insert_rowid};
+
+#[derive(Queryable, Debug)]
 pub struct VacationType {
     pub id: i32,
     pub name: String,
     pub count: bool,
 }
-#[derive(Insertable)]
+
+impl Display for VacationType {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        if f.alternate() {
+            write!(f, "<VacationType {}>", self.name)
+        } else {
+            write!(
+                f,
+                "<VacationType {} (Do count: {} | ID: {})>",
+                self.name, self.count, self.id
+            )
+        }
+    }
+}
+
+impl VacationType {
+    pub fn new<'a>(name: &'a str, count: &'a bool) -> NewVacationType<'a> {
+        NewVacationType { name, count }
+    }
+}
+
+#[derive(Insertable, Debug)]
 #[table_name = "vacation_types"]
 pub struct NewVacationType<'a> {
     pub name: &'a str,
     pub count: &'a bool,
 }
 
-impl VacationType {
-    pub fn new<'a>(name: &'a str, count: &'a bool) -> NewVacationType<'a> {
-        NewVacationType { name, count }
+impl Display for NewVacationType<'_> {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        if f.alternate() {
+            write!(f, "<NewVacationType {}>", self.name)
+        } else {
+            write!(
+                f,
+                "<NewVacationType {} (Do count: {})>",
+                self.name, self.count
+            )
+        }
+    }
+}
+
+impl NewVacationType<'_> {
+    #[instrument(skip(self, conn))]
+    pub fn save_to_db(self, conn: &SqliteConnection) -> Result<i32, Error> {
+        debug!(target: "new_db_entry", "Adding to db: {:?}", &self);
+        diesel::insert_into(vacation_types::table)
+            .values(&self)
+            .execute(conn)?;
+
+        trace!(target: "new_db_entry", "Get `last_insert_rowid` for id of: {:#}", &self);
+        let id = diesel::select(last_insert_rowid).get_result::<i32>(conn);
+        debug!(target: "new_db_entry", "Got ID: <{:?}> for {:#}", id, &self);
+        id
     }
 }
