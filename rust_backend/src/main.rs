11a99810768c8c5a8dde99c608e0a8ef9da0c7bb
Christian Riedel <cielquan@protonmail.com>
2021-03-18 11:02:57 +0100

    add log file writer

--- rust_backend/src/main.rs
+++ rust_backend/src/main.rs
@@ -2,21 +2,29 @@
 extern crate diesel;
 extern crate dotenv;
 extern crate tracing;
+extern crate tracing_appender;
 extern crate tracing_subscriber;
 
 mod db;
 use db::models::*;
 
-fn init_tracing() {
-    tracing_subscriber::fmt()
-        .pretty()
-        .with_thread_names(true)
-        .with_max_level(tracing::Level::TRACE)
-        .init();
-}
-
 fn main() {
-    init_tracing();
+    let file_appender = tracing_appender::rolling::daily("logs", "log");
+    let (non_blocking, _guard) = tracing_appender::non_blocking(file_appender);
+    tracing_subscriber::fmt()
+        .with_writer(non_blocking)
+        .pretty()
+        .with_ansi(false)
+        .with_level(true)
+        .with_target(true)
+        .with_thread_names(true)
+        .with_thread_ids(true)
+        .with_max_level(tracing::Level::TRACE)
+        // .reload_handle(true);
+        .init();
+    tracing::error!("Test tracing");
+
+    tracing::trace!("App start");
 
     let conn = db::establish_connection();
 
@@ -28,5 +36,5 @@ fn main() {
 }
 
 fn create_new_user<'a>() -> NewUser<'a> {
-    User::new("Name1", &32, &12, None)
+    User::new("Name11", &32, &123, None)
 }
