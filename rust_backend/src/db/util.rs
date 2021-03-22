fb0e5275b00bef4a419f7c860bd294313ace0f77
Christian Riedel <cielquan@protonmail.com>
2021-03-22 06:30:27 +0100

    renamed establish_connection and make more generic

--- rust_backend/src/db/util.rs
+++ rust_backend/src/db/util.rs
@@ -1,13 +1,8 @@
 use diesel::prelude::*;
-use dotenv::dotenv;
-use std::env;
 
-pub fn establish_connection() -> SqliteConnection {
-    dotenv().ok();
-
-    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
-    SqliteConnection::establish(&database_url)
-        .unwrap_or_else(|_| panic!("Error connecting to {}", database_url))
+pub fn establish_connection_to(db_url: &str) -> SqliteConnection {
+    SqliteConnection::establish(&db_url)
+        .unwrap_or_else(|_| panic!("Error connecting to {}", db_url))
 }
 
 no_arg_sql_function!(
