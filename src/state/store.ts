ab97cd5f9f82c62f03a7d8fcdd3e1e5c97786d45
Christian Riedel <cielquan@protonmail.com>
2021-06-20 16:35:44 +0200

    add redux-devtools-extension

--- src/state/store.ts
+++ src/state/store.ts
@@ -1,7 +1,9 @@
 import { createStore, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
+import { composeWithDevTools } from "redux-devtools-extension/logOnlyInProduction";
+
 import rootReducer from "./reducers";
 
-const store = createStore(rootReducer, {}, applyMiddleware(thunk));
+const store = createStore(rootReducer, {}, composeWithDevTools(applyMiddleware(thunk)));
 
 export default store;
