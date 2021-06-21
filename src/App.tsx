bf2cb7210eada9ccda754974d2da8d50611f3f17
Christian Riedel <cielquan@protonmail.com>
2021-06-21 13:18:03 +0200

    add locale to theme

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { useSelector } from "react-redux";
 import { CssBaseline } from "@material-ui/core";
 import { ThemeProvider } from "@material-ui/core/styles";
+import * as locales from "@material-ui/core/locale";
 
 import { State } from "./state";
 import useStyles from "./styles";
@@ -13,8 +14,9 @@ import SideMenu from "./components/SideMenu";
 
 function App(): React.ReactElement {
   const darkState = useSelector((state: State) => state.darkTheme);
+  const langState = useSelector((state: State) => state.language);
 
-  const theme = createTheme(darkState);
+  const theme = createTheme(darkState, locales[langState.locale]);
   const classes = useStyles();
 
   return (
--- src/theme.ts
+++ src/theme.ts
@@ -1,20 +1,24 @@
 import { createMuiTheme, Theme } from "@material-ui/core/styles";
 import { orange, lightBlue, deepOrange, deepPurple } from "@material-ui/core/colors";
+import { Localization } from "@material-ui/core/locale";
 
-const createTheme = (darkState: boolean): Theme => {
+const createTheme = (darkState: boolean, language: Localization): Theme => {
   const palletType = darkState ? "dark" : "light";
 
-  return createMuiTheme({
-    palette: {
-      type: palletType,
-      primary: {
-        main: darkState ? orange[500] : lightBlue[500],
-      },
-      secondary: {
-        main: darkState ? deepOrange[900] : deepPurple[500],
+  return createMuiTheme(
+    {
+      palette: {
+        type: palletType,
+        primary: {
+          main: darkState ? orange[500] : lightBlue[500],
+        },
+        secondary: {
+          main: darkState ? deepOrange[900] : deepPurple[500],
+        },
       },
     },
-  });
+    language
+  );
 };
 
 export default createTheme;
