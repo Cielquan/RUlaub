d8b9b81e6167fc96402ce10d5bf61aa0aa3bcd29
Christian Riedel <cielquan@protonmail.com>
2021-07-05 18:17:45 +0200

    fix type for initialState for localConfigReducer

--- src/state/reducers/localConfigReducer.ts
+++ src/state/reducers/localConfigReducer.ts
@@ -3,7 +3,7 @@ import { LocalConfigAction } from "../actions";
 import { LocalConfig, updateLocalConfig } from "../utils/localConfig";
 import localConfig from "../../dev_temp/test.local_config.json";
 
-const initialState: LocalConfig = localConfig;
+const initialState = localConfig as LocalConfig;
 
 const reducer = (
   state: LocalConfig = initialState,
