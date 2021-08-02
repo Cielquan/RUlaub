53127fda24322611114746e436bf56687999b217
Christian Riedel <cielquan@protonmail.com>
2021-08-02 11:34:03 +0200

    fix <p> in <p> react warning

--- src/components/InfoPage.tsx
+++ src/components/InfoPage.tsx
@@ -54,13 +54,13 @@ const InfoPage = (): ReactElement => {
       <DialogTitle>{`RUlaub v${pjson.version}`}</DialogTitle>
       <DialogContent>
         <DialogContentText className={classes.helpPageText}>
-          <Typography>
+          <Typography component="span">
             <Trans>
               RUlaub is licensed under either &apos;Apache License 2.0&apos; or
               &apos;MIT License&apos; at your option.
             </Trans>
           </Typography>
-          <Typography>
+          <Typography component="span">
             <Trans>
               For the Source Code, Documentation and copies of the License files please
               see the github repository at: {ghLink}.
