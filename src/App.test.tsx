1ddb9e427fe7e4c491aaec01ad45da37f0eb8924
Christian Riedel <cielquan@protonmail.com>
2021-08-02 14:53:07 +0200

    add tests for App component

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,49 @@
+import { render, RenderResult } from "@testing-library/react";
 import React from "react";
-import { render, screen } from "@testing-library/react";
-import App from "./App";
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
+import configureMockStore from "redux-mock-store";
 
-test("renders learn react link", () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+import App from "./App";
+import Languages from "./i18n";
+
+const mockStore = configureMockStore([thunk]);
+
+describe("Base App", () => {
+  let store;
+  let component: RenderResult;
+
+  beforeEach(() => {
+    store = mockStore({
+      dbData: { users: [] },
+      infoPage: false,
+      language: Languages.english,
+      localConfig: { settings: { yearToShow: 2021 } },
+      sideMenu: false,
+      sideMenuDatabase: false,
+      theme: "dark",
+    });
+
+    component = render(
+      <Provider store={store}>
+        <App />
+      </Provider>
+    );
+  });
+
+  it("renders root element", async () => {
+    const { findByTestId } = component;
+
+    const element = await findByTestId("rulaub-root");
+
+    expect(element).toBeInTheDocument();
+  });
+
+  it("renders main element", async () => {
+    const { findByTestId } = component;
+
+    const element = await findByTestId("rulaub-main");
+
+    expect(element).toBeInTheDocument();
+  });
 });
