c289b577e28f168e50b20cf720b0a69413a7f529
Christian Riedel <cielquan@protonmail.com>
2021-08-03 07:07:55 +0200

    add test for default language vars

---
+++ src/i18n.test.ts
@@ -0,0 +1,9 @@
+import Languages, { defaultLanguage, defaultLocale } from "./i18n";
+
+test("default i18n locale is en-US", () => {
+  expect(defaultLocale).toEqual("en-US");
+});
+
+test("default i18n language is correctly picked", () => {
+  expect(defaultLanguage).toEqual(Languages.english);
+});
