69a2afeb46fd262300281e079de00852da3016d6
Christian Riedel <cielquan@protonmail.com>
2021-08-04 12:43:56 +0200

    update testUtils

---
+++ src/testUtils.ts
@@ -0,0 +1,15 @@
+import configureMockStore, { MockStoreEnhanced } from "redux-mock-store";
+import thunk from "redux-thunk";
+
+import { State } from "./state";
+
+const generateMockStore = (
+  state: Partial<State>
+): MockStoreEnhanced<Partial<State>> => {
+  const mockStore = configureMockStore([thunk]);
+  const store = mockStore(state) as MockStoreEnhanced<Partial<State>>;
+  store.clearActions();
+  return store;
+};
+
+export default generateMockStore;
