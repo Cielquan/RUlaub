25359b40327f2acbc3e9e92b9500e100041d4bda
Christian Riedel <cielquan@protonmail.com>
2021-08-04 12:44:21 +0200

    change App test to snapshot test

---
+++ src/__tests__/App.snapshot.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { Provider } from "react-redux";
+import renderer from "react-test-renderer";
+
+import App from "../App";
+import { initialState } from "../state";
+import generateMockStore from "../testUtils";
+
+jest.mock("../components/Calendar", () => () => "Calendar");
+jest.mock("../components/InfoPage", () => () => "InfoPage");
+jest.mock("../components/Navbar", () => () => "Navbar");
+jest.mock("../components/NewHolidayButton", () => () => "NewHolidayButton");
+jest.mock("../components/SideMenu", () => () => "SideMenu");
+
+describe("Base App", () => {
+  const mockStore = generateMockStore(initialState);
+
+  beforeEach(() => {
+    mockStore.clearActions();
+  });
+
+  it("renders correctly with mocked subComponents", () => {
+    const tree = renderer
+      .create(
+        <Provider store={mockStore}>
+          <App />
+        </Provider>
+      )
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
