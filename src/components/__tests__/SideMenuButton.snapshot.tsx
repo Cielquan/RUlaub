d2651519752d20a818e8182d0a6e2886d0415cf4
Christian Riedel <cielquan@protonmail.com>
2021-08-05 09:50:34 +0200

    add snapshot test for SideMenuButton

---
+++ src/components/__tests__/SideMenuButton.snapshot.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { Provider } from "react-redux";
+import renderer from "react-test-renderer";
+
+import SideMenuButton from "../SideMenuButton";
+import { initialState } from "../../state";
+import generateMockStore from "../../testUtils";
+
+describe("SideMenuButton component", () => {
+  const mockStore = generateMockStore(initialState);
+
+  beforeEach(() => {
+    mockStore.clearActions();
+  });
+
+  it("renders correctly", () => {
+    const tree = renderer
+      .create(
+        <Provider store={mockStore}>
+          <SideMenuButton key="key" text="text" onClick={() => undefined} />
+        </Provider>
+      )
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
