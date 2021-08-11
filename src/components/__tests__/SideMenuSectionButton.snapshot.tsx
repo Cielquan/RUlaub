41c78433f5d501e04acdeed64848626e2a9d8494
Christian Riedel <cielquan@protonmail.com>
2021-08-11 08:31:50 +0200

    update SideMenu buttons snapshot tests

--- src/components/__tests__/SideMenuSectionButton.snapshot.tsx
+++ src/components/__tests__/SideMenuSectionButton.snapshot.tsx
@@ -2,13 +2,13 @@ import React from "react";
 import { Provider } from "react-redux";
 import renderer from "react-test-renderer";
 
-import SideMenuDatabaseModifyButton from "../SideMenuDatabaseModifyButton";
+import SideMenuSectionButton from "../SideMenuSectionButton";
 import { initialState } from "../../state";
 import generateMockStore from "../../testUtils";
 
 jest.mock("../SideMenuButton", () => () => "SideMenuButton");
 
-describe("SideMenuDatabaseModifyButton component", () => {
+describe("SideMenuSectionButton component", () => {
   const mockStore = generateMockStore(initialState);
 
   beforeEach(() => {
@@ -19,7 +19,7 @@ describe("SideMenuDatabaseModifyButton component", () => {
     const tree = renderer
       .create(
         <Provider store={mockStore}>
-          <SideMenuDatabaseModifyButton />
+          <SideMenuSectionButton text="text" listKey="key" sectionItemList={[]} />
         </Provider>
       )
       .toJSON();
