28fd4e687e66a57cb47b5290aa25bf32c370851e
Christian Riedel <cielquan@protonmail.com>
2021-08-11 14:24:13 +0200

    mocked and unmocked test for SideMenuSectionButton

--- src/components/__tests__/SideMenuSectionButton.snapshot.tsx
+++ src/components/__tests__/SideMenuSectionButton.snapshot.tsx
@@ -2,20 +2,37 @@ import React from "react";
 import { Provider } from "react-redux";
 import renderer from "react-test-renderer";
 
+import SideMenuButton from "../SideMenuButton";
 import SideMenuSectionButton from "../SideMenuSectionButton";
 import { initialState } from "../../state";
 import generateMockStore from "../../testUtils";
 
-jest.mock("../SideMenuButton", () => () => "SideMenuButton");
+jest.mock("../SideMenuButton", () => ({ __esModule: true, default: jest.fn() }));
 
 describe("SideMenuSectionButton component", () => {
   const mockStore = generateMockStore(initialState);
 
   beforeEach(() => {
     mockStore.clearActions();
+
+    (SideMenuButton as unknown as jest.Mock).mockImplementation(
+      jest.requireActual("../SideMenuButton").default
+    );
   });
 
-  it("renders correctly with mocked subComponents", () => {
+  it("renders correctly (unmocked)", () => {
+    const tree = renderer
+      .create(
+        <Provider store={mockStore}>
+          <SideMenuSectionButton text="text" listKey="key" sectionItemList={[]} />
+        </Provider>
+      )
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it("renders correctly with string-mocked subComponents", () => {
+    (SideMenuButton as unknown as jest.Mock).mockImplementation(() => "SideMenuButton");
     const tree = renderer
       .create(
         <Provider store={mockStore}>
