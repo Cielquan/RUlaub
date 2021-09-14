cb5e1ebf3617620a2462844e493a390bcb8715c9
Christian Riedel <cielquan@protonmail.com>
2021-09-14 15:29:45 +0200

    calc additional style const

--- src/styles.ts
+++ src/styles.ts
@@ -2,13 +2,19 @@ import { Theme } from "@mui/material/styles";
 import { CSSSelectorObject, SxProps, SystemStyleObject } from "@mui/system";
 
 export const STYLE_CONST = {
-  CALENDAR_COLUMN_WIDTH: 35,
+  CALENDAR_COLUMN_WIDTH: 32,
   CALENDAR_ROW_HEIGHT: 35,
-  CALENDAR_COLUMN_WIDTH_FULL: 32 + 2, // incl. gutter size
-  CALENDAR_ROW_HEIGHT_FULL: 35 + 2, // incl. gutter size
   CALENDAR_GUTTER_SIZE: 2,
+  CALENDAR_COLUMN_WIDTH_FULL: 0, // incl. gutter size
+  CALENDAR_ROW_HEIGHT_FULL: 0, // incl. gutter size
   CALENDAR_ROW_LABEL_WIDTH: 250,
 };
+
+STYLE_CONST.CALENDAR_COLUMN_WIDTH_FULL =
+  STYLE_CONST.CALENDAR_COLUMN_WIDTH + STYLE_CONST.CALENDAR_GUTTER_SIZE;
+
+STYLE_CONST.CALENDAR_ROW_HEIGHT_FULL =
+  STYLE_CONST.CALENDAR_ROW_HEIGHT + STYLE_CONST.CALENDAR_GUTTER_SIZE;
 
 export type SxStyle =
   | SxProps<Theme>
