2331ad93d1a75bc147194707d600d92d1812b237
Christian Riedel <cielquan@protonmail.com>
2021-09-17 12:16:42 +0200

    remove outcommented code

--- src/state/utils/vacationData.ts
+++ src/state/utils/vacationData.ts
@@ -1,12 +1,3 @@
-// interface Vacation {
-//   id: number;
-//   startDate: string;
-//   startYearDay: number;
-//   endDate: string;
-//   endYearDay: number;
-//   hexcolor: number;
-// }
-
 import { UserData, VacationDataSchema } from "../../types/vacationData.schema";
 
 export type VacationData = VacationDataSchema;
