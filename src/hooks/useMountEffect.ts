33e40fa88943154b22ace80f1df01441aa74635e
Christian Riedel <cielquan@protonmail.com>
2021-09-21 07:16:21 +0200

    rename useMountEffect param more clearly

--- src/hooks/useMountEffect.ts
+++ src/hooks/useMountEffect.ts
@@ -2,6 +2,6 @@ import { useEffect } from "react";
 
 // Empty array is need to run hook only on mount
 // eslint-disable-next-line react-hooks/exhaustive-deps
-const useMountEffect = (fn: () => void): void => useEffect(fn, []);
+const useMountEffect = (callback: () => void): void => useEffect(callback, []);
 
 export default useMountEffect;
