83bac24f3ba4b0ecc336e92a2336b99a7c4d0120
Christian Riedel <cielquan@protonmail.com>
2021-09-22 11:09:44 +0200

    add username to navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,21 @@
-import { Menu as MenuIcon } from "@mui/icons-material";
-import { AppBar, IconButton, Toolbar, Typography } from "@mui/material";
+import { Menu as MenuIcon, Person as PersonIcon } from "@mui/icons-material";
+import {
+  AppBar,
+  Avatar,
+  IconButton,
+  List,
+  ListItem,
+  ListItemAvatar,
+  ListItemText,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+import { Box } from "@mui/system";
 import React, { ReactElement } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { bindActionCreators } from "redux";
 
-import { actionCreators } from "../state";
+import { actionCreators, State } from "../state";
 
 import DarkThemeSwitch from "./DarkThemeSwitch";
 import LanguageMenu from "./LanguageMenu";
@@ -12,6 +23,8 @@ import LanguageMenu from "./LanguageMenu";
 const Navbar = (): ReactElement => {
   const dispatch = useDispatch();
   const { openSideMenu } = bindActionCreators(actionCreators, dispatch);
+  const configState = useSelector((state: State) => state.config);
+  const themeState = configState.settings.theme;
 
   return (
     <AppBar position="fixed">
@@ -27,6 +40,22 @@ const Navbar = (): ReactElement => {
         <Typography sx={{ flexGrow: 1 }} variant="h6" align="left" noWrap>
           RUlaub
         </Typography>
+        <List sx={{ paddingRight: 2 }}>
+          <ListItem
+            sx={{
+              border: "1px solid",
+              borderColor: themeState === "dark" ? "primary.main" : "action.active",
+              borderRadius: 2,
+            }}
+          >
+            <ListItemAvatar>
+              <Avatar>
+                <PersonIcon />
+              </Avatar>
+            </ListItemAvatar>
+            <ListItemText primary={configState.user.name} />
+          </ListItem>
+        </List>
         <LanguageMenu />
         <DarkThemeSwitch />
       </Toolbar>
