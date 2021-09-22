efca15c6015c54b3c92433c7210e38d1696ac6d3
Christian Riedel <cielquan@protonmail.com>
2021-09-22 12:14:11 +0200

    secure CalendarRowLabelsUserCell against empty array

--- src/components/CalendarRowLabelsUserCell.tsx
+++ src/components/CalendarRowLabelsUserCell.tsx
@@ -30,7 +30,7 @@ const CalendarRowLabelsUserCell = ({ index: rowIndex, style }: Props): ReactElem
       }}
     >
       <Typography sx={{ padding: "0 0.3em" }} variant="body1" noWrap>
-        {vacationDataState[rowIndex].name}
+        {vacationDataState[rowIndex]?.name}
       </Typography>
     </Box>
   );
