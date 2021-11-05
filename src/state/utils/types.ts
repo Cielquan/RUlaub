5a0880063b4f5d82288c1219092fc042e07d362c
Christian Riedel <cielquan@protonmail.com>
2021-11-05 13:23:07 +0100

    add NewVacation types

--- src/state/utils/types.ts
+++ src/state/utils/types.ts
@@ -1,6 +1,6 @@
 import { PublicHolidayData } from "../../backendAPI/types/publicHolidaysData.schema";
 import { SchoolHolidayData } from "../../backendAPI/types/schoolHolidaysData.schema";
-import { UserData } from "../../backendAPI/types/usersData.schema";
+import { UserData, VacationData } from "../../backendAPI/types/usersData.schema";
 import { VacationTypeData } from "../../backendAPI/types/vacationTypesData.schema";
 
 export type PublicHolidayDataPayload = [string, PublicHolidayData];
@@ -14,6 +14,10 @@ export type NewSchoolHolidayDataPayload = [string, NewSchoolHolidayData];
 export type UserDataPayload = [string, UserData];
 export type NewUserData = Omit<UserData, "calc" | "vacations">;
 export type NewUserDataPayload = [string, NewUserData];
+
+export type VacationDataPayload = [string, VacationData];
+export type NewVacationData = VacationData;
+export type NewVacationDataPayload = [string, NewVacationData];
 
 export type VacationTypeDataPayload = [string, VacationTypeData];
 export type NewVacationTypeData = VacationTypeData;
