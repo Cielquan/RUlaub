d3282050eed2e265d437b9d1c06ece145e674749
Christian Riedel <cielquan@protonmail.com>
2021-11-09 17:44:11 +0100

    simplified config loader

--- rust_backend_test/src/config/loader.rs
+++ rust_backend_test/src/config/loader.rs
@@ -1,106 +1,63 @@
-use std::collections::HashMap;
-use std::io::{Error as IOError, ErrorKind as IOErrorKind};
-use std::path::{Path, PathBuf};
-use std::sync::mpsc::channel;
-use std::sync::RwLock;
-use std::time::Duration;
+use std::{
+    collections::HashMap,
+    path::Path,
+    sync::{mpsc::channel, RwLock},
+    time::Duration,
+};
 
 use configlib::*;
 use directories::ProjectDirs;
 use notify::{DebouncedEvent, RecommendedWatcher, RecursiveMode, Watcher};
-use tracing::{debug, error, info, trace};
-
-/*
-1. Build path
-2. Check path
-3. Check if file
-4. Get as String
-
-*/
-
 use thiserror::Error;
+use tracing::{debug, error, info, trace};
 
 #[derive(Error, Debug)]
 pub enum ConfigError {
-    #[error("Could not build config file path.")]
-    PathNotFound,
+    #[error("Could not find project dirs.")]
+    ProjectDirsNotFound,
     #[error("Could not stringify config file path.")]
     PathStringify,
 }
 
-fn build_conf_file_path() -> Option<PathBuf> {
-    Some(
-        ProjectDirs::from("", crate::AUTHOR, crate::NAME)?
-            .config_dir()
-            .join("settings.toml"),
-    )
-}
-
-fn get_conf_file_path() -> Result<PathBuf, ConfigError> {
-    match build_conf_file_path() {
-        Some(path) => {
-            trace!("Built conf file path: <{:?}>", path);
-            Ok(path)
-        }
+#[tracing::instrument]
+fn get_conf_file_path() -> Result<String, ConfigError> {
+    match ProjectDirs::from("", crate::AUTHOR, crate::NAME) {
         None => {
-            error!(concat!(
-                "Failed to build conf file path. ",
-                "Potential issue with the filesystem. ",
-                "Could not find project direcotries for this OS."
-            ));
-            Err(ConfigError::PathNotFound)
+            error!("Could not find project directories for this OS.");
+            Err(ConfigError::ProjectDirsNotFound)
+        }
+        Some(project_dirs) => {
+            match project_dirs.config_dir().join("settings.toml").to_str() {
+                None => {
+                    error!(concat!(
+                        "Failed to build conf file path. ",
+                        "Could not stringify the path."
+                    ));
+                    Err(ConfigError::PathStringify)
+                }
+                Some(path) => {
+                    trace!("Built conf file path: <{}>", path);
+                    Ok(String::from(path))
+                }
+            }
         }
     }
 }
 
-fn check_conf_file_path_is_file() -> bool {
-    match get_conf_file_path() {
-        Some(path) => {
-            debug!("Config file <{:?}> does exist.", path);
-            return Path::new(&path).is_file();
-        }
-        None => false,
-    }
-}
-
-fn get_conf_file_path_as_string() -> Result<String, ConfigError> {
-    if let Some(path) = get_conf_file_path()?.to_str() {
-        return Ok(String::from(path));
-    }
-    Err(ConfigError::PathStringify)
-}
-
-fn check_conf_file_path() -> PathBuf {
-    get_conf_file_path().expect({
-        let err = concat!(
-            "Potential issue with the filesystem. ",
-            "Could not find project direcotries for this OS."
-        );
-        error!(err);
-        err
-    })
+fn check_path_is_file(path: &str) -> bool {
+    trace!("Check if path <{:?}> points to file.", path);
+    let is_file = Path::new(path).is_file();
+    debug!("Path <{:?}> points to file: {}", path, is_file);
+    is_file
 }
 
 lazy_static! {
     #[derive(Debug)]
-    pub static ref SETTINGS_FILE_PATH: PathBuf = {
-        check_conf_file_path()
-    };
-    pub static ref SETTINGS_FILE_PATH_STR: String = {
-        if let Some(path) = SETTINGS_FILE_PATH.to_str() {
-            return String::from(path)
-        }
-        String::new()
-    };
-    pub static ref SETTINGS_FILE_FOUND: bool = {
-        if let Ok(path) = get_conf_file_path() {
-            return Path::new(&path).is_file()
-        }
-        false
-    };
+    pub static ref SETTINGS_FILE_PATH_STR: String = get_conf_file_path().unwrap_or_default();
+    pub static ref SETTINGS_FILE_FOUND: bool = check_path_is_file(&SETTINGS_FILE_PATH_STR);
     pub static ref SETTINGS: RwLock<Config> = RwLock::new({
         let mut settings = Config::default();
-        settings.merge(File::with_name(&SETTINGS_FILE_PATH_STR[..])).expect({
+        settings.merge(File::with_name(&SETTINGS_FILE_PATH_STR)).expect({
             let err = "Failed to merge config file into config struct.";
             error!(err);
             err
