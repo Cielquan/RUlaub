d45cca1b9f0d10364e1c22a6bead015c4e2c4ce1
Christian Riedel <cielquan@protonmail.com>
2021-11-20 15:51:30 +0100

    add tracing to backend

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,8 +3,25 @@
     windows_subsystem = "windows"
 )]
 
+#[macro_use]
+extern crate tracing;
+
+use rulaub_backend::logging::logger::start_tracer;
+use tauri::Event;
+
 fn main() {
-    tauri::Builder::default()
-        .run(tauri::generate_context!())
-        .expect("error while running tauri application");
+    let (_tracing_handle, _guard) = start_tracer();
+    info!("App started.");
+    info!("Logging level: TRACE");
+
+    let app = tauri::Builder::default()
+        .build(tauri::generate_context!())
+        .expect("error while building tauri application");
+
+    app.run(|_app_handle, event| match event {
+        Event::Exit => info!("### App ending."),
+        _ => (),
+    });
+
+    info!("App ended.");
 }
