e1fb0de26a7c0ee7923520879671445e8a6db540
Christian Riedel <cielquan@protonmail.com>
2021-11-21 16:12:01 +0100

    add start loading screen

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -6,8 +6,11 @@
 #[macro_use]
 extern crate tracing;
 
+use std::{thread::sleep, time::Duration};
+
+use tauri::{Event, Manager};
+
 use rulaub_backend::logging::logger::start_tracer;
-use tauri::Event;
 
 fn main() {
     let (_tracing_handle, _guard) = start_tracer();
@@ -15,6 +18,22 @@ fn main() {
     info!("Logging level: TRACE");
 
     let app = tauri::Builder::default()
+        .setup(|app| {
+            trace!("Start app setup.");
+            let loadingscreen_window = app.get_window("loadingscreen").unwrap();
+            let main_window = app.get_window("main").unwrap();
+
+            tauri::async_runtime::spawn(async move {
+                trace!("Start app init.");
+                sleep(Duration::from_secs(2));
+                trace!("Finish app init.");
+
+                loadingscreen_window.close().unwrap();
+                main_window.show().unwrap();
+            });
+            trace!("Finished app setup.");
+            Ok(())
+        })
         .build(tauri::generate_context!())
         .expect("error while building tauri application");
 
