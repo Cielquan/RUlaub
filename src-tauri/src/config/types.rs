b4c1ea29d597d2efb9393408731ef862977078a5
Christian Riedel <cielquan@protonmail.com>
2021-12-07 07:26:32 +0100

    add enums for config values with fixed array of values

--- src-tauri/src/config/types.rs
+++ src-tauri/src/config/types.rs
@@ -22,3 +22,81 @@ pub struct Config {
     pub user: Option<User>,
     pub settings: Settings,
 }
+
+#[derive(Debug, Clone, Deserialize, Serialize)]
+pub enum Language {
+    DE,
+    EN,
+}
+
+impl Language {
+    pub fn to_string(&self) -> String {
+        match *self {
+            Language::DE => "de-DE".to_string(),
+            Language::EN => "en-US".to_string(),
+        }
+    }
+
+    pub fn to_enum(value: &str) -> Self {
+        match value {
+            "de-DE" => Language::DE,
+            "en-US" => Language::EN,
+            _ => Language::DE,
+        }
+    }
+}
+
+#[derive(Debug, Clone, Deserialize, Serialize)]
+pub enum LogLevel {
+    TRACE,
+    DEBUG,
+    INFO,
+    WARN,
+    ERROR,
+}
+
+impl LogLevel {
+    pub fn to_string(&self) -> String {
+        match *self {
+            LogLevel::TRACE => "trace".to_string(),
+            LogLevel::DEBUG => "debug".to_string(),
+            LogLevel::INFO => "info".to_string(),
+            LogLevel::WARN => "warn".to_string(),
+            LogLevel::ERROR => "error".to_string(),
+        }
+    }
+
+    pub fn to_enum(value: &str) -> Self {
+        match value {
+            "trace" => LogLevel::TRACE,
+            "debug" => LogLevel::DEBUG,
+            "info" => LogLevel::INFO,
+            "warn" => LogLevel::WARN,
+            "error" => LogLevel::ERROR,
+            _ => LogLevel::INFO,
+        }
+    }
+}
+
+#[derive(Debug, Clone, Deserialize, Serialize)]
+pub enum Theme {
+    DARK,
+    LIGHT,
+}
+
+impl Theme {
+    pub fn to_string(&self) -> String {
+        match *self {
+            Theme::DARK => "dark".to_string(),
+            Theme::LIGHT => "light".to_string(),
+        }
+    }
+
+    pub fn to_enum(value: &str) -> Self {
+        match value {
+            "dark" => Theme::DARK,
+            "light" => Theme::LIGHT,
+            _ => Theme::DARK,
+        }
+    }
+}
