e5cc641a7ef9d6d78016fd7beef367dd7799bf57
Christian Riedel <cielquan@protonmail.com>
2021-12-08 09:31:33 +0100

    rename param in setter

--- src-tauri/src/commands/state/set.rs
+++ src-tauri/src/commands/state/set.rs
@@ -14,51 +14,51 @@ use crate::state::{
 };
 
 #[tauri::command]
-pub fn set_config_state(data: Config, state: tauri::State<ConfigState>) {
-    *state.0.lock() = data;
+pub fn set_config_state(config: Config, state: tauri::State<ConfigState>) {
+    *state.0.lock() = config;
 }
 
 #[tauri::command]
-pub fn set_db_uri(data: String, state: tauri::State<ConfigState>) {
-    state.0.lock().settings.database_uri = Some(data);
+pub fn set_db_uri(db_uri: String, state: tauri::State<ConfigState>) {
+    state.0.lock().settings.database_uri = Some(db_uri);
 }
 
 #[tauri::command]
-pub fn set_langauge(data: Language, state: tauri::State<ConfigState>) {
-    state.0.lock().settings.language = LanguageData::new(data);
+pub fn set_langauge(lang: Language, state: tauri::State<ConfigState>) {
+    state.0.lock().settings.language = LanguageData::new(lang);
 }
 
 #[tauri::command]
-pub fn set_log_level(data: LogLevel, state: tauri::State<ConfigState>) {
-    state.0.lock().settings.log_level = data;
+pub fn set_log_level(level: LogLevel, state: tauri::State<ConfigState>) {
+    state.0.lock().settings.log_level = level;
 }
 
 #[tauri::command]
-pub fn set_theme(data: Theme, state: tauri::State<ConfigState>) {
-    state.0.lock().settings.theme = data;
+pub fn set_theme(theme: Theme, state: tauri::State<ConfigState>) {
+    state.0.lock().settings.theme = theme;
 }
 
 #[tauri::command]
-pub fn set_public_holidays_state(data: PublicHolidays, state: tauri::State<PublicHolidaysState>) {
-    *state.0.lock() = data;
+pub fn set_public_holidays_state(pub_holiday: PublicHolidays, state: tauri::State<PublicHolidaysState>) {
+    *state.0.lock() = pub_holiday;
 }
 
 #[tauri::command]
-pub fn set_school_holidays_state(data: SchoolHolidays, state: tauri::State<SchoolHolidaysState>) {
-    *state.0.lock() = data;
+pub fn set_school_holidays_state(school_holiday: SchoolHolidays, state: tauri::State<SchoolHolidaysState>) {
+    *state.0.lock() = school_holiday;
 }
 
 #[tauri::command]
-pub fn set_user_row_map_state(data: UserRowMap, state: tauri::State<UserRowMapState>) {
-    *state.0.lock() = data;
+pub fn set_user_row_map_state(user_row_map: UserRowMap, state: tauri::State<UserRowMapState>) {
+    *state.0.lock() = user_row_map;
 }
 
 #[tauri::command]
-pub fn set_users_state(data: Users, state: tauri::State<UsersState>) {
-    *state.0.lock() = data;
+pub fn set_users_state(users: Users, state: tauri::State<UsersState>) {
+    *state.0.lock() = users;
 }
 
 #[tauri::command]
-pub fn set_vacation_types_state(data: VacationTypes, state: tauri::State<VacationTypesState>) {
-    *state.0.lock() = data;
+pub fn set_vacation_types_state(vac_types: VacationTypes, state: tauri::State<VacationTypesState>) {
+    *state.0.lock() = vac_types;
 }
