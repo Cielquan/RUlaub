b91517c5ddc794c487d1752d7632b715290f116b
Christian Riedel <cielquan@protonmail.com>
2021-12-14 14:47:02 +0100

    add serializer for config to toml str

--- src-tauri/src/config/parser.rs
+++ src-tauri/src/config/parser.rs
@@ -1,3 +1,5 @@
+use crate::config::types::ConfigFile;
+
 use super::Config;
 
 /// Parse toml string into [`ConfigFile`] struct. Then transform it into [`Config`] struct.
@@ -16,5 +18,23 @@ pub fn parse_toml_str_to_config(toml_str: &str) -> anyhow::Result<Config> {
             return Err(err.into());
         }
         Ok(conf) => Ok(Config::from_configfile(conf)),
+    }
+}
+
+pub fn serialize_config_to_toml_str(conf: Config) -> anyhow::Result<String> {
+    debug!(
+        target = "config",
+        message = "Try to serialize Config struct into toml string"
+    );
+    match toml::to_string::<ConfigFile>(&ConfigFile::from_config(conf.clone())) {
+        Err(err) => {
+            error!(
+                target = "config",
+                message = "Failed to serialize ConfigFile struct into toml string",
+                error = ?err
+            );
+            return Err(err.into());
+        }
+        Ok(conf) => Ok(conf),
     }
 }
