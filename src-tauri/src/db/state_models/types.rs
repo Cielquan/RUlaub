9c2ac1d5f565121e945bc41280ed0f7d52c28422
Christian Riedel <cielquan@protonmail.com>
2021-12-23 21:53:01 +0100

    add date type conversion

--- src-tauri/src/db/state_models/types.rs
+++ src-tauri/src/db/state_models/types.rs
@@ -1,3 +1,5 @@
+use chrono::NaiveDate;
+
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct DateData {
     pub date: ISODate,
@@ -6,3 +8,23 @@ pub struct DateData {
 }
 
 pub type ISODate = String;
+
+pub fn naive_date_to_iso_date(naive_date: NaiveDate) -> ISODate {
+    naive_date.format("%Y-%m-%d").to_string()
+}
+
+pub fn iso_date_to_naive_date(iso_date: ISODate) -> Option<NaiveDate> {
+    trace!(target = "db", message = "Parse ISO date string", iso_date = ?iso_date);
+    match NaiveDate::parse_from_str(&iso_date, "%Y-%m-%d") {
+        Err(err) => {
+            error!(
+                target = "db",
+                message = "Failed to parse ISO date string",
+                iso_date = ?iso_date,
+                error = ?err,
+            );
+            None
+        }
+        Ok(date) => Some(date),
+    }
+}
