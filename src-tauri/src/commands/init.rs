9c79a4a76cb76d535982f7a0694e3ca2c3c0ea65
Christian Riedel <cielquan@protonmail.com>
2021-12-23 16:17:54 +0100

    make init command async

--- src-tauri/src/commands/init.rs
+++ src-tauri/src/commands/init.rs
@@ -1,9 +1,13 @@
 //! Commands used while the programm's init
+use super::CommandResult;
 use crate::state::status_states::PageInit;
 use crate::state::PageInitState;
 
 #[tracing::instrument(skip(page_init_state))]
 #[tauri::command]
-pub fn finished_init_load(page_init_state: tauri::State<PageInitState>) {
+pub async fn finished_init_load(
+    page_init_state: tauri::State<'_, PageInitState>,
+) -> CommandResult<()> {
     *page_init_state.0.lock() = PageInit::DONE;
+    Ok(())
 }
