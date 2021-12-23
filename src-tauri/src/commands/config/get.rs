ef9d4133adabfa6b2b83b87b82c510b8504c8e86
Christian Riedel <cielquan@protonmail.com>
2021-12-23 16:15:16 +0100

    make config getter async

--- src-tauri/src/commands/config/get.rs
+++ src-tauri/src/commands/config/get.rs
@@ -1,6 +1,7 @@
 //! Commands to get data from the config state managed by tauri from the frontend
 use std::collections::HashMap;
 
+use crate::commands::CommandResult;
 use crate::config::language::LanguageData;
 use crate::config::theme::Theme;
 use crate::config::{Config, AVAILABLE_LANGUAGES, AVAILABLE_THEMES};
@@ -10,42 +11,42 @@ use crate::state::ConfigState;
 
 #[tracing::instrument(skip(state))]
 #[tauri::command]
-pub fn get_config_state(state: tauri::State<ConfigState>) -> Config {
-    state.0.lock().clone()
+pub async fn get_config_state(state: tauri::State<'_, ConfigState>) -> CommandResult<Config> {
+    Ok(state.0.lock().clone())
 }
 
 #[tracing::instrument(skip(state))]
 #[tauri::command]
-pub fn get_language(state: tauri::State<ConfigState>) -> LanguageData {
-    state.0.lock().settings.language.clone()
+pub async fn get_language(state: tauri::State<'_, ConfigState>) -> CommandResult<LanguageData> {
+    Ok(state.0.lock().settings.language.clone())
 }
 
 #[tracing::instrument]
 #[tauri::command]
-pub fn get_available_languages() -> HashMap<&'static str, LanguageData> {
-    (*AVAILABLE_LANGUAGES).clone()
+pub async fn get_available_languages() -> CommandResult<HashMap<&'static str, LanguageData>> {
+    Ok((*AVAILABLE_LANGUAGES).clone())
 }
 
 #[tracing::instrument(skip(state))]
 #[tauri::command]
-pub fn get_log_level(state: tauri::State<ConfigState>) -> LogLevel {
-    state.0.lock().settings.log_level.clone()
+pub async fn get_log_level(state: tauri::State<'_, ConfigState>) -> CommandResult<LogLevel> {
+    Ok(state.0.lock().settings.log_level.clone())
 }
 
 #[tracing::instrument]
 #[tauri::command]
-pub fn get_available_log_levels() -> [&'static str; 5] {
-    *AVAILABLE_LOG_LEVELS
+pub async fn get_available_log_levels() -> CommandResult<[&'static str; 5]> {
+    Ok(*AVAILABLE_LOG_LEVELS)
 }
 
 #[tracing::instrument(skip(state))]
 #[tauri::command]
-pub fn get_theme(state: tauri::State<ConfigState>) -> Theme {
-    state.0.lock().settings.theme.clone()
+pub async fn get_theme(state: tauri::State<'_, ConfigState>) -> CommandResult<Theme> {
+    Ok(state.0.lock().settings.theme.clone())
 }
 
 #[tracing::instrument]
 #[tauri::command]
-pub fn get_available_themes() -> [&'static str; 2] {
-    *AVAILABLE_THEMES
+pub async fn get_available_themes() -> CommandResult<[&'static str; 2]> {
+    Ok(*AVAILABLE_THEMES)
 }
