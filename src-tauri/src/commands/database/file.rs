647908433fd4ef05823b59918d045953b256b375
Christian Riedel <cielquan@protonmail.com>
2022-01-06 07:36:35 +0100

    impl create_db command

--- src-tauri/src/commands/database/file.rs
+++ src-tauri/src/commands/database/file.rs
@@ -1,9 +1,13 @@
 use crate::commands::CommandResult;
+use crate::db::migrate_db_schema;
 
 /// Create a new SQLite database for RUlaub.
 #[tracing::instrument]
 #[tauri::command]
 pub async fn create_db(path: String) -> CommandResult<()> {
+    if migrate_db_schema(&path, true).is_err() {
+        return Err("Failed to create new database or migrate existing one.".into());
+    }
     Ok(())
 }
 
