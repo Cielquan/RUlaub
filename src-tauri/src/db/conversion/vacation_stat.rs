bdf0e12c2a2a41d317558117a6089b85d891dd69
Christian Riedel <cielquan@protonmail.com>
2022-01-15 20:19:39 +0100

    add fn map_user_workdays_list

--- src-tauri/src/db/conversion/vacation_stat.rs
+++ src-tauri/src/db/conversion/vacation_stat.rs
@@ -1,8 +1,8 @@
 use std::collections::HashMap;
 
-use chrono::NaiveDate;
+use chrono::{NaiveDate, Weekday};
 
-use crate::db::query_only_models::VacationWithType;
+use crate::db::query_only_models::{self, VacationWithType};
 use crate::db::state_models::user::UserId;
 use crate::db::state_models::vacation_type::VacationTypeId;
 
@@ -32,4 +32,43 @@ pub fn group_vacations(vacation_list: Vec<VacationWithType>) -> VacationMap {
         })
     }
     user_vac_map
+}
+
+pub fn map_user_workdays_list(
+    user_workdays_list: &Vec<query_only_models::UserWorkdays>,
+) -> HashMap<i32, Vec<Weekday>> {
+    trace!(
+        target = "database-data",
+        message = "Build a list of user workdays from user database data",
+    );
+
+    let mut user_workdays: HashMap<i32, Vec<Weekday>> = HashMap::new();
+
+    user_workdays_list.iter().for_each(|user| {
+        let mut workday_list = Vec::new();
+        if user.monday {
+            workday_list.push(Weekday::Mon);
+        }
+        if user.tuesday {
+            workday_list.push(Weekday::Tue);
+        }
+        if user.wednesday {
+            workday_list.push(Weekday::Wed);
+        }
+        if user.thursday {
+            workday_list.push(Weekday::Thu);
+        }
+        if user.friday {
+            workday_list.push(Weekday::Fri);
+        }
+        if user.saturday {
+            workday_list.push(Weekday::Sat);
+        }
+        if user.sunday {
+            workday_list.push(Weekday::Sun);
+        }
+        user_workdays.insert(user.id, workday_list);
+    });
+
+    user_workdays
 }
