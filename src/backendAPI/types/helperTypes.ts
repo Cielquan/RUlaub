5f25705aef585141f1b0539b4f36eb5fe5ad95ae
Christian Riedel <cielquan@protonmail.com>
2022-01-16 21:09:51 +0100

    use number type for keys in map types for updated data

--- src/backendAPI/types/helperTypes.ts
+++ src/backendAPI/types/helperTypes.ts
@@ -1,38 +1,40 @@
-import {
-  PublicHolidayData,
-  PublicHolidaysDataSchema,
-} from "./publicHolidaysData.schema";
-import {
-  SchoolHolidayData,
-  SchoolHolidaysDataSchema,
-} from "./schoolHolidaysData.schema";
-import { UserData, UsersDataSchema } from "./usersData.schema";
+import { PublicHolidayData } from "./publicHolidaysData.schema";
+import { SchoolHolidayData } from "./schoolHolidaysData.schema";
+import { UserData } from "./usersData.schema";
 import { VacationData } from "./vacationsData.schema";
-import { VacationTypeData, VacationTypesDataSchema } from "./vacationTypesData.schema";
+import { VacationTypeData } from "./vacationTypesData.schema";
 
 export type PublicHolidayDataPayload = [string, PublicHolidayData];
-export type PublicHolidayDataMap = PublicHolidaysDataSchema;
+export interface PublicHolidayDataMap {
+  [k: number]: PublicHolidayData;
+}
 export type NewPublicHolidayData = Omit<PublicHolidayData, "calc">;
 export type NewPublicHolidayDataPayload = [string, NewPublicHolidayData];
 
 export type SchoolHolidayDataPayload = [string, SchoolHolidayData];
-export type SchoolHolidayDataMap = SchoolHolidaysDataSchema;
+export interface SchoolHolidayDataMap {
+  [k: number]: SchoolHolidayData;
+}
 export type NewSchoolHolidayData = SchoolHolidayData;
 export type NewSchoolHolidayDataPayload = [string, NewSchoolHolidayData];
 
 export type UserDataPayload = [string, UserData];
-export type UserDataMap = UsersDataSchema;
+export interface UserDataMap {
+  [k: number]: UserData;
+}
 export type NewUserData = Omit<UserData, "calc">;
 export type NewUserDataPayload = [string, NewUserData];
 
 export type VacationDataPayload = [string, VacationData];
 export interface VacationDataMap {
-  [k: string]: VacationData;
+  [k: number]: VacationData;
 }
 export type NewVacationData = VacationData;
 export type NewVacationDataPayload = [string, NewVacationData];
 
 export type VacationTypeDataPayload = [string, VacationTypeData];
-export type VacationTypeDataMap = VacationTypesDataSchema;
+export interface VacationTypeDataMap {
+  [k: number]: VacationTypeData;
+}
 export type NewVacationTypeData = VacationTypeData;
 export type NewVacationTypeDataPayload = [string, NewVacationTypeData];
