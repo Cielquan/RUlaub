eb08315848df374940d2369ad559c4e5c138df7d
Christian Riedel <cielquan@protonmail.com>
2022-01-16 17:52:37 +0100

    add to db model conversion for school holiday

--- src-tauri/src/db/conversion/school_holiday.rs
+++ src-tauri/src/db/conversion/school_holiday.rs
@@ -3,7 +3,7 @@ use std::collections::HashMap;
 use super::super::models;
 use super::super::state_models::types::DateData;
 use super::super::state_models::{self, SchoolHoliday};
-use super::date_type::naive_date_to_iso_date;
+use super::date_type::{iso_date_to_naive_date, naive_date_to_iso_date};
 
 pub fn to_state_model(db_data: Vec<models::SchoolHoliday>) -> state_models::SchoolHolidays {
     trace!(
@@ -33,4 +33,87 @@ pub fn to_state_model(db_data: Vec<models::SchoolHoliday>) -> state_models::Scho
     }
 
     map
+}
+
+pub fn to_new_db_model(
+    new_entries: &Vec<state_models::SchoolHoliday>,
+) -> (Vec<models::NewSchoolHoliday>, u32) {
+    trace!(
+        target = "database-data",
+        message = "Convert new state data to SchoolHoliday db models",
+    );
+
+    let mut error_count = 0;
+    let mut db_models = Vec::new();
+
+    for entry in new_entries {
+        let start_date = match iso_date_to_naive_date(entry.start.date.clone()) {
+            Err(_) => {
+                error_count += 1;
+                continue;
+            }
+            Ok(date) => date,
+        };
+        let end_date = match iso_date_to_naive_date(entry.end.date.clone()) {
+            Err(_) => {
+                error_count += 1;
+                continue;
+            }
+            Ok(date) => date,
+        };
+
+        db_models.push(models::SchoolHoliday::create_new_entry(
+            &entry.name,
+            start_date,
+            &entry.start.year_day,
+            &entry.start.year,
+            end_date,
+            &entry.end.year_day,
+            &entry.end.year,
+        ))
+    }
+
+    (db_models, error_count)
+}
+
+pub fn to_update_db_model(
+    updated_entries: state_models::SchoolHolidays,
+) -> (Vec<models::SchoolHoliday>, u32) {
+    trace!(
+        target = "database-data",
+        message = "Convert updated state data to SchoolHoliday db models",
+    );
+
+    let mut error_count = 0;
+    let mut db_models = Vec::new();
+
+    for (id, entry) in updated_entries {
+        let start_date = match iso_date_to_naive_date(entry.start.date) {
+            Err(_) => {
+                error_count += 1;
+                continue;
+            }
+            Ok(date) => date,
+        };
+        let end_date = match iso_date_to_naive_date(entry.end.date) {
+            Err(_) => {
+                error_count += 1;
+                continue;
+            }
+            Ok(date) => date,
+        };
+
+        db_models.push(models::SchoolHoliday::create_update_entry(
+            id,
+            entry.name,
+            start_date,
+            entry.start.year_day,
+            entry.start.year,
+            end_date,
+            entry.end.year_day,
+            entry.end.year,
+        ))
+    }
+
+    (db_models, error_count)
 }
