c1da6730d2aa67e92031c94cb47ec929f8b1b079
Christian Riedel <cielquan@protonmail.com>
2022-01-16 18:00:01 +0100

    add to db model conversion for vac type

--- src-tauri/src/db/conversion/vacation_type.rs
+++ src-tauri/src/db/conversion/vacation_type.rs
@@ -26,3 +26,50 @@ pub fn to_state_model(db_data: Vec<models::VacationType>) -> state_models::Vacat
 
     map
 }
+
+pub fn to_new_db_model(
+    new_entries: &Vec<state_models::VacationType>,
+) -> Vec<models::NewVacationType> {
+    trace!(
+        target = "database-data",
+        message = "Convert new state data to VacationType db models",
+    );
+
+    let mut db_models = Vec::new();
+
+    for entry in new_entries {
+        db_models.push(models::VacationType::create_new_entry(
+            &entry.name,
+            &entry.charge,
+            &entry.color_dark,
+            &entry.color_light,
+            &entry.active,
+        ))
+    }
+
+    db_models
+}
+
+pub fn to_update_db_model(
+    updated_entries: state_models::VacationTypes,
+) -> Vec<models::VacationType> {
+    trace!(
+        target = "database-data",
+        message = "Convert updated state data to VacationType db models",
+    );
+
+    let mut db_models = Vec::new();
+
+    for (id, entry) in updated_entries {
+        db_models.push(models::VacationType::create_update_entry(
+            id,
+            entry.name,
+            entry.charge,
+            entry.color_dark,
+            entry.color_light,
+            entry.active,
+        ))
+    }
+
+    db_models
+}
